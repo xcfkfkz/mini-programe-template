cee6dfff9e00d6fd7576f7c8a30b2f77c26e1210
xcfkfkz <xcfkfkz@hotmail.com>
2021-09-25 23:47:01 +0800

    【提交类型】缺陷修改 【提交内容】authCode与phone降级逻辑 【评审人】wzj

--- apis/http/index.js
+++ apis/http/index.js
@@ -7,7 +7,11 @@ export const loginWithAliAuthCode = async (authCode) => {
     const path = '/users/login/alipay';
     my.request({
       url: host + path,
-      headers: {},
+      headers: {
+        'X-Request-Package-ID': '1000',
+        'X-Request-Device': 'web',
+        'X-Request-Version': 'none'
+      },
       method: 'POST',
       data: {
         auth_code: authCode
@@ -15,15 +19,64 @@ export const loginWithAliAuthCode = async (authCode) => {
       timeout: 0,
       dataType: 'json',
       success: (result) => {
-        
+        resolve(result)
       },
-      fail: () => {
-        
-      },
-      complete: () => {
-        
+      fail: (err) => {
+        resolve(err)
       }
     });
-  })
-
-})
+  });
+  return res
+};
+export const loadCollectCodeInfo = async (sellerCode) => {
+  const envName = await getEnvName();
+  const res = await new Promise((resolve, reject) => {
+    const host = hosts[envName].GAPI;
+    const path = '/org/collect_code/info';
+    my.request({
+      url: host + path,
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      method: 'GET',
+      data: {
+        u: sellerCode
+      },
+      success: (result) => {
+        resolve(result)
+      },
+      fail: (err) => {
+        resolve(err)
+      }
+    })
+  });
+  return res
+};
+export const loginByPhone = async (phone) => {
+  const envName = await getEnvName();
+  const res = await new Promise((resolve, reject) => {
+    const host = hosts[envName].GAPI;
+    const path = '/login/phone';
+    my.request({
+      url: host + path,
+      headers: {
+        'X-Request-Package-ID': '1000',
+        'X-Request-Device': 'web',
+        'X-Request-Version': 'none'
+      },
+      method: 'POST',
+      data: {
+        auth_code: phone
+      },
+      timeout: 0,
+      dataType: 'json',
+      success: (result) => {
+        resolve(result)
+      },
+      fail: (err) => {
+        resolve(err)
+      }
+    });
+  });
+  return res
+}
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,38 +1,75 @@
-import 
+import {loginWithAliAuthCode, loadCollectCodeInfo, loginByPhone} from '../../apis/http/index.js';
 Page({
   data: {
     avatar: '',
     nickname: '',
-    url: ''
+    url: '',
+    needApprovePhone: false
   },
   onLoad(query) {
     const { webViewUrl:url = '' } = query || {};
     this.setData({url});
-
+    const { u = '' } = this.parseQueryString(url.split('?')[1]);
+    loadCollectCodeInfo(u)
+      .then(res => {        
+        const { avatar = '', userName:nickname = '' } = (res && res.info) || {};
+        this.setData({avatar, nickname})
+      })
   },
-
-  pay(e) {
-    const url = e.target.dataset.url;
+  goWithToken(t) {
+    my.navigateTo({ url: '../webView/webView?webViewUrl=' + encodeURIComponent(`${this.data.url}&token=${t || ''}`) });
+  },
+  pay() {
     my.getAuthCode({
       scopes: ['auth_user'],
-      success: res => {
+      success: async (res) => {
         // 获取到authCode
-        console.log('this ', this);
-        
+        const { token = '' } = (await loginWithAliAuthCode(res && res.authCode)) || {};
+        if (token) return this.goWithToken(token);
+        this.setData({needApprovePhone: true})
       },
       fail: () => {
         // 没获取到authCode
-        my.navigateTo({ url: '../webView/webView?webViewUrl=' + encodeURIComponent(url) })
+        this.goWithToken()
       }
     })
   },
+  cancel() {
+    this.goWithToken()
+  },
+  popApprovePhone() {
+    this.setData({needApprovePhone: false})
+  },
   onGetAuthorize() {
-    console.log('成功回调')
+    console.log('用户同意授权手机号')
+    my.getPhoneNumber({
+      success: async (res) => {
+        const encryptedData = res.response;
+        console.log('获得加密手机号', encryptedData)
+        const { token = '' } = (await loginByPhone(encryptedData)) || {};
+        if (token) return this.goWithToken(token);
+        this.goWithToken()
+      },
+      fail: (res) => {
+        console.log('同意但获取失败', res);
+        this.goWithToken()
+      },
+    });
+
   },
   onAuthError() {
-    console.log('失败回调')
+    console.log('用户拒绝授权手机号');
+    this.goWithToken()
   },
-  loginWithAliAuthCode() {
-
-  }
+  parseQueryString(qs) {    
+    return qs
+      ? qs.split('&').reduce((acc, s) => {
+          const tokens = s.split('=')
+          return {
+            ...acc,
+            [tokens[0]]: tokens[1]
+          }
+        }, {})
+      : {}
+  },
 });
